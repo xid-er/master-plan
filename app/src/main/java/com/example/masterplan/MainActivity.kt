3052f0ec8ecc34a04da15704acd4a4ed52542091
Kārlis Siders <karlis.siders@gmail.com>
2026-05-29 00:18:57 +0100

    Make UI sleeker with dark purple theme and no top bar, dim non-active tab icons

--- app/src/main/java/com/example/masterplan/MainActivity.kt
+++ app/src/main/java/com/example/masterplan/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.masterplan
 
 import android.os.Bundle
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -18,6 +21,16 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.statusBarColor = android.graphics.Color.TRANSPARENT
+        ViewCompat.setOnApplyWindowInsetsListener(binding.toolbar) { view, insets ->
+            val top = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top
+            view.setPadding(view.paddingLeft, top, view.paddingRight, view.paddingBottom)
+            insets
+        }
+        WindowCompat.getInsetsController(window, window.decorView)
+            .isAppearanceLightStatusBars = false
 
         val navView: BottomNavigationView = binding.navView
 
